59780d875437e99761b217ee64c6e9a07d4c25f0
Vinidev <vinicius.coding@gmail.com>
2024-03-25 14:35:42 -0300

    Projects url implemented
    - url_launcher package added
    - urls setted in project_view.dart
    - get_it package removed
    - project_card_component_old.dart removed

--- portfolio_vinidev/lib/app/dashboard/projects/component/project_card_component.dart
+++ portfolio_vinidev/lib/app/dashboard/projects/component/project_card_component.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio_vinidev/app/core/theme/portfolio_color_scheme.dart';
 import 'package:portfolio_vinidev/app/core/theme/portfolio_theme.dart';
--- portfolio_vinidev/lib/app/dashboard/projects/project_view.dart
+++ portfolio_vinidev/lib/app/dashboard/projects/project_view.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio_vinidev/app/core/theme/portfolio_color_scheme.dart';
 import 'package:portfolio_vinidev/app/core/theme/portfolio_theme.dart';
 import 'package:portfolio_vinidev/app/dashboard/projects/model/project.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectView extends StatefulWidget {
   final void Function()? onBack;
@@ -24,22 +25,24 @@ class _ProjectViewState extends State<ProjectView> {
     Project(
       title: "Protechtor Gear",
       description:
-          "The Plus+ application is an application designed to improve management capacity in establishments.  Its main functionality is to display a real-time counter on the screen, which can be incremented when a customer enters the establishment, and decremented when a customer leaves the establishment. When the capacity limit is reached, Plus+ notifies the user that the place has reached the maximum possible capacity and blocks the Enter button.",
+          "Protechtor Gears is your password filtering application, with it you can filter strong passwords in a list, add a password and see in real time whether or not it meets the strong password criteria, edit and delete passwords. Don't waste time and protect yourself in the tech world with Protechtor Gears.",
       features:
-          "• Dynamic Counter to manage the capacity in your establishment\n• Set the capacity limit for the counting\n• Set a background image\n• Your settings will be saved\n",
-      githubURL: "",
-      linkedinURL: "",
+          "• Passwords filterings.\n• API connection.\n• Creation, reading, update and deletion of passwords.\n• Passwords validation with RegEx.\n",
+      githubURL: "https://github.com/Vini-Coding/Protechtor-Gears/",
+      linkedinURL:
+          "https://www.linkedin.com/feed/update/urn:li:activity:7073009128876482560/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7073009128876482560%29",
       minLogoPath: 'assets/images/projects/min_protechtor_gear.png',
       primaryProjectColor: const Color(0xFF530149),
     ),
     Project(
       title: "BMI Check",
       description:
-          "The Plus+ application is an application designed to improve management capacity in establishments.  Its main functionality is to display a real-time counter on the screen, which can be incremented when a customer enters the establishment, and decremented when a customer leaves the establishment. When the capacity limit is reached, Plus+ notifies the user that the place has reached the maximum possible capacity and blocks the Enter button.",
+          "Welcome to BMI Check! With BMI Check you can calculate your body mass index and discover if you are in your ideal body weight. It provides you a custom text with health recommendations, what is your ideal body weight and allows you to share with your friends in social media. You can also change the height and weight metrics, switch between dark and light theme and choose your language.",
       features:
-          "• Dynamic Counter to manage the capacity in your establishment\n• Set the capacity limit for the counting\n• Set a background image\n• Your settings will be saved\n",
-      githubURL: "",
-      linkedinURL: "",
+          "• BMI Calculation.\n• Ideal Body Weight and Custom Text for each BMI.\n• Share your result in social media.\n• Switch between light and dark theme.\n• Change the language, height and weight metrics.",
+      githubURL: "https://github.com/Vini-Coding/BMI-Check/",
+      linkedinURL:
+          "https://www.linkedin.com/posts/vin%C3%ADcius-soares-584075255_flutter-dart-mobiledevelopment-activity-7063878350980734976-ebLK?utm_source=share&utm_medium=member_desktop",
       minLogoPath: 'assets/images/projects/min_bmi_check.png',
       primaryProjectColor: const Color(0xFF1ACB21),
     ),
@@ -48,13 +51,26 @@ class _ProjectViewState extends State<ProjectView> {
       description:
           "The Plus+ application is an application designed to improve management capacity in establishments.  Its main functionality is to display a real-time counter on the screen, which can be incremented when a customer enters the establishment, and decremented when a customer leaves the establishment. When the capacity limit is reached, Plus+ notifies the user that the place has reached the maximum possible capacity and blocks the Enter button.",
       features:
-          "• Dynamic Counter to manage the capacity in your establishment\n• Set the capacity limit for the counting\n• Set a background image\n• Your settings will be saved\n",
-      githubURL: "",
-      linkedinURL: "",
+          "• Dynamic Counter to manage the capacity in your establishment.\n• Set the capacity limit for the counting.\n• Set a background image.\n• Your settings will be saved.\n",
+      githubURL: "https://github.com/Vini-Coding/PlusPlus/",
+      linkedinURL:
+          "https://www.linkedin.com/posts/vin%C3%ADcius-soares-584075255_plus-application-demo-video-the-plus-application-activity-6999016679712133120-jQY_?utm_source=share&utm_medium=member_desktop",
       minLogoPath: 'assets/images/projects/min_plus+.png',
       primaryProjectColor: const Color(0xFFBD2020),
     ),
   ];
+
+  void launchLink(String linkUrl) async {
+    final url = Uri.parse(linkUrl);
+
+    if (await canLaunchUrl(url)) {
+      launchUrl(
+        url,
+      );
+    } else {
+      debugPrint("URL lanch failed");
+    }
+  }
 
   @override
   void initState() {
@@ -103,21 +119,25 @@ class _ProjectViewState extends State<ProjectView> {
                   SizedBox(width: screenSize.width * 0.005),
                   IconButton(
                     padding: const EdgeInsets.symmetric(horizontal: 0),
-                    onPressed: () {},
+                    onPressed: () async => launchLink(
+                      projects[currentProjectIndex].githubURL,
+                    ),
                     icon: FaIcon(
                       FontAwesomeIcons.github,
                       size: screenSize.width * 0.02,
-                      color: projects[currentProjectIndex].primaryProjectColor,
+                      color: portfolioColorScheme.onBackground,
                     ),
                   ),
                   SizedBox(width: screenSize.width * 0.005),
                   IconButton(
                     padding: const EdgeInsets.all(0),
-                    onPressed: () {},
+                    onPressed: () async => launchLink(
+                      projects[currentProjectIndex].linkedinURL,
+                    ),
                     icon: FaIcon(
                       FontAwesomeIcons.linkedin,
                       size: screenSize.width * 0.02,
-                      color: projects[currentProjectIndex].primaryProjectColor,
+                      color: portfolioColorScheme.onBackground,
                     ),
                   ),
                   SizedBox(width: screenSize.width * 0.005),
@@ -153,8 +173,11 @@ class _ProjectViewState extends State<ProjectView> {
               ),
               const Spacer(),
               InkWell(
-                onTap: () {},
+                onTap: () async => launchLink(
+                  projects[currentProjectIndex].githubURL,
+                ),
                 child: Row(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     Image.asset(
                       projects[currentProjectIndex].minLogoPath,
