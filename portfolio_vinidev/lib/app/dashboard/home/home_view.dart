7bfda8c34c8f228d023e71dccff2fa3aa2c4fbef
Vinidev <vinicius.coding@gmail.com>
2024-04-21 19:38:31 -0300

    HomeMobileView()
    - home mobile view dashboard

--- portfolio_vinidev/lib/app/dashboard/home/home_view.dart
+++ portfolio_vinidev/lib/app/dashboard/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_vinidev/app/core/theme/portfolio_theme.dart';
 import 'package:portfolio_vinidev/app/core/utils/responsive_layout_builder.dart';
 import 'package:portfolio_vinidev/app/dashboard/home/desktop/home_desktop_view.dart';
+import 'package:portfolio_vinidev/app/dashboard/home/mobile/home_mobile_view.dart';
 import 'package:portfolio_vinidev/app/dashboard/home/tablet/home_tablet_view.dart';
 
 class HomeView extends StatefulWidget {
@@ -18,7 +19,7 @@ class _HomeViewState extends State<HomeView> {
       backgroundColor: portfolioTheme.backgroundColor,
       desktop: const HomeDesktopView(),
       tablet: const HomeTabletView(),
-      mobile: const SizedBox(),
+      mobile: const HomeMobileView(),
     );
   }
 }
--- portfolio_vinidev/lib/app/dashboard/home/mobile/home_mobile_view.dart
+++ portfolio_vinidev/lib/app/dashboard/home/mobile/home_mobile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_vinidev/app/core/utils/exports/theme_exports.dart';
 
 class HomeMobileView extends StatefulWidget {
   const HomeMobileView({super.key});
@@ -10,6 +11,76 @@ class HomeMobileView extends StatefulWidget {
 class _HomeMobileViewState extends State<HomeMobileView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          'assets/images/dash/dash_home.png',
+          width: screenSize.width * 0.9,
+        ),
+        RichText(
+          textAlign: TextAlign.start,
+          text: TextSpan(
+            style: portfolioTheme.textTheme.displayLarge?.copyWith(
+              fontSize: screenSize.width * 0.065,
+            ),
+            text: "Hello world, ",
+            children: [
+              TextSpan(
+                text: "you found it",
+                style: portfolioTheme.textTheme.displayLarge?.copyWith(
+                  fontSize: screenSize.width * 0.065,
+                  color: portfolioTheme.primaryColor,
+                ),
+              ),
+              const TextSpan(text: "."),
+            ],
+          ),
+        ),
+        RichText(
+          textAlign: TextAlign.justify,
+          text: TextSpan(
+            style: portfolioTheme.textTheme.bodyMedium?.copyWith(
+              fontWeight: FontWeight.w800,
+              fontSize: screenSize.width * 0.03,
+            ),
+            text:
+                "If you are looking for someone who could bring your ideas and concepts to the fingertips of users with ",
+            children: [
+              TextSpan(
+                text: "maestry in Flutter",
+                style: portfolioTheme.textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.w900,
+                  fontSize: screenSize.width * 0.03,
+                  color: portfolioTheme.primaryColor,
+                ),
+              ),
+              const TextSpan(text: ", "),
+              TextSpan(
+                text: "clean code ",
+                style: portfolioTheme.textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.w900,
+                  fontSize: screenSize.width * 0.03,
+                  color: portfolioTheme.primaryColor,
+                ),
+              ),
+              const TextSpan(text: "and "),
+              TextSpan(
+                text: "best user experience as possible",
+                style: portfolioTheme.textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.w900,
+                  fontSize: screenSize.width * 0.03,
+                  color: portfolioTheme.primaryColor,
+                ),
+              ),
+              const TextSpan(text: ", you found it!"),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
